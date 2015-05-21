0b7c19899a0f67ecfcf43093948f22ff91ce44bd
unknown <aboxer@ABOXER-LT.ida.org>
2015-05-21 17:16:58 -0400

    modified connection files

--- early/config/connect.php
+++ early/config/connect.php
@@ -2,7 +2,7 @@
 
 DEFINE ("DB_HOST", "localhost");
 DEFINE ("DB_USER", "idolsoft_acb");
-DEFINE ("DB_PASSWORD", "Marmot32");
+DEFINE ("DB_PASSWORD", "---");
 DEFINE ("DB_NAME", "idolsoft_db1");
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ("Could not connect to the database because: " . mysqli_connect_error() );
