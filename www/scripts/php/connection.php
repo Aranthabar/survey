1273a423975bb85f43aa4e7fec3ad516a319e992
Justace Clutter <jclutter@ida.org>
2015-05-22 00:00:21 -0400

    Add and alter the connection.php file to have the correct username and password

---
+++ www/scripts/php/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+DEFINE ("DB_HOST", "localhost");
+DEFINE ("DB_USER", "testacct");
+DEFINE ("DB_PASSWORD", "testacct");
+DEFINE ("DB_NAME", "idolsoft_db1");
+
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ("Could not connect to the database because: " . mysqli_connect_error() );
+
+?>
